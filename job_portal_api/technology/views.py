5fe14bd012e0ced78ac47306dcc0b8d8465ee776
achyut kadam <achyut.k@acldigital.com>
2022-11-09 17:13:20 +0530

    Added delete feature, but not working, and Create 50 records using faker

--- job_portal_api/technology/views.py
+++ job_portal_api/technology/views.py
@@ -1,5 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_list_or_404, get_object_or_404
+from faker import Faker
+
+import time
+import random
 
 from .models import Technology
 from .serializers import TechnologySerializer
@@ -12,9 +17,20 @@ class TechnologyView(APIView):
 
   def post(self, request):
     technology = request.data.get('technology')
-    serializer = TechnologySerializer(data=technology)
-    if serializer.is_valid(raise_exception=True):
-      technology_saved = serializer.save()
+    faker = Faker()
+    for n in range(1, 50):
+      technology['name'] = faker.name()
+      technology['description'] = faker.text()
+      technology['is_active'] = random.choice([True, False])
+
+      print("+++++++++++++")
+      print(technology)
+      print("+++++++++++++")
+      serializer = TechnologySerializer(data=technology)
+      if serializer.is_valid(raise_exception=True):
+        technology_saved = serializer.save()
+      # time.sleep(1.5)
+
     return Response({"success": "Technology '{}' created successfully". format(technology_saved.name)})
 
   def put(self, request, pk):
@@ -24,3 +40,10 @@ class TechnologyView(APIView):
     if serializer.is_valid(raise_exception=True):
       technology_saved = serializer.save()
     return Response({"success": "Technology '{}' updated successfully". format(technology_saved.name)})
+
+
+  def delete(self, request, pk):
+    # Get object with this pk
+    technology = get_object_or_404(Technology.objects.all(), pk=pk)
+    technology.delete()
+    return Response({"message": "Technology with id `{}` has been deleted.".format(pk)},status=204)

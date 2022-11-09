1a97fccc96b04b2b98f616ca92a7d98fe3b9b8b7
achyut kadam <achyut.k@acldigital.com>
2022-11-09 16:02:42 +0530

    Added Create and Update Operation using API view

--- job_portal_api/technology/urls.py
+++ job_portal_api/technology/urls.py
@@ -4,5 +4,6 @@ from .views import TechnologyView
 app_name = "technology"
 
 urlpatterns = [
-    path('technology/', TechnologyView.as_view()),
+  path('technology/', TechnologyView.as_view()),
+  path('technology/<int:pk>', TechnologyView.as_view())
 ]
--- job_portal_api/technology/views.py
+++ job_portal_api/technology/views.py
@@ -5,7 +5,22 @@ from .models import Technology
 from .serializers import TechnologySerializer
 
 class TechnologyView(APIView):
-    def get(self, request):
-        technologys = Technology.objects.all()
-        serializer = TechnologySerializer(technologys, many=True)
-        return Response({'technologys': serializer.data})
+  def get(self, request):
+    technologys = Technology.objects.all()
+    serializer = TechnologySerializer(technologys, many=True)
+    return Response({'technologys': serializer.data})
+
+  def post(self, request):
+    technology = request.data.get('technology')
+    serializer = TechnologySerializer(data=technology)
+    if serializer.is_valid(raise_exception=True):
+      technology_saved = serializer.save()
+    return Response({"success": "Technology '{}' created successfully". format(technology_saved.name)})
+
+  def put(self, request, pk):
+    saved_technology = get_object_or_404(Technology.objects.all(), pk=pk)
+    data = request.data.get('technlology')
+    serializer = TechnologySerializer(isinstance=saved_technology, data=data, partial=True)
+    if serializer.is_valid(raise_exception=True):
+      technology_saved = serializer.save()
+    return Response({"success": "Technology '{}' updated successfully". format(technology_saved.name)})

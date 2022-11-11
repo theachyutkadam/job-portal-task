96b609fc69d47fa02313f72b0747a4eec10e4083
achyut kadam <achyut.k@acldigital.com>
2022-11-11 12:30:15 +0530

    configure new course app model in project

--- job_portal_api/job_portal_api/settings.py
+++ job_portal_api/job_portal_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'technology.apps.TechnologyConfig',
+    'course.apps.CourseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- job_portal_api/job_portal_api/urls.py
+++ job_portal_api/job_portal_api/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('technology.urls')),
+    path('', include('course.urls')),
 ]

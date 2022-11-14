ed3f7323578614c2a2c859f2b8222dc8221df008
achyut kadam <achyut.k@acldigital.com>
2022-11-14 16:57:54 +0530

    Remove unwanted lines

--- job_portal_api/course/urls.py
+++ job_portal_api/course/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import CourseList, CourseDetail
 
-app_name = "course"
-
 urlpatterns = [
   path('course/', CourseList.as_view()),
   path('course/<int:pk>', CourseDetail.as_view())

3369a6582488f2ac11a01aacf18c5e33ff83fe80
achyut kadam <achyut.k@acldigital.com>
2022-11-16 11:30:03 +0530

    Show course_id in program model index

--- job_portal_api/program/serializers.py
+++ job_portal_api/program/serializers.py
@@ -1,10 +1,12 @@
 from dataclasses import fields
 from rest_framework import serializers
 from .models import Program
-from course.serializers import CourseSerializer
+# from course.serializers import CourseSerializer
 
 class ProgramSerializer(serializers.HyperlinkedModelSerializer):
-  course = CourseSerializer()
+  # course = CourseSerializer()  #User the CourseSerializer when you want show object of the course.
   class Meta:
     model = Program
-    fields = ('id', 'name', 'description', 'course', 'detail_overview', 'cover_image', 'banner_image', 'is_active', 'learning_mode', 'start_date', 'end_date', 'training_option', 'type', 'seats', 'feature', 'requirement', 'eligitbility', 'tools_coverd', 'skill', 'is_free_course', 'meta_keywords', 'meta_description')
+    fields = ('name', 'url', 'description', 'course_id', 'detail_overview', 'cover_image', 'banner_image', 'is_active',
+              'learning_mode', 'start_date', 'end_date', 'training_option', 'type', 'seats', 'feature', 'requirement',
+              'eligitbility', 'tools_coverd', 'skill', 'is_free_course', 'meta_keywords', 'meta_description')
